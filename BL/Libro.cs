dd4d18c92ab6f7e6d83830a80b1ed96c52d5c37a
MiguelJuarezz <jomiguelpejuarez@gmail.com>
2022-12-09 19:02:47 -0600

    update

--- BL/Libro.cs
+++ BL/Libro.cs
@@ -124,5 +124,75 @@ namespace BL
             return result;
 
         }
+
+
+        public static ML.Result Update(ML.Libro libro)
+        {
+            //instancia de result
+            ML.Result result = new ML.Result();
+            try
+            {
+                //SqlCnnnection es para la conexion a sql server
+                using (SqlConnection context = new SqlConnection(DL.Conexion.GetConnectionString()))
+                {
+                    //almacenar y ejecutar querys de sql
+                    using (SqlCommand cmd = new SqlCommand())
+                    {
+                        cmd.CommandText = "LibroUpdate";
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Connection = context;
+                        //ya tiene la sentancia y la conexion, hacen falta parametros 
+
+                        //Agregar parametros
+                        SqlParameter[] collection = new SqlParameter[8];
+
+                        collection[0] = new SqlParameter("@IdLibro", SqlDbType.Int);
+                        collection[0].Value = libro.Nombre;
+                        collection[1] = new SqlParameter("@Nombre", SqlDbType.VarChar);
+                        collection[1].Value = libro.Nombre;
+                        collection[2] = new SqlParameter("@IdAutor", SqlDbType.Int);
+                        collection[2].Value = libro.Autor.IdAutor;
+                        collection[3] = new SqlParameter("@NumeroPaginas", SqlDbType.Int);
+                        collection[3].Value = libro.NumeroPaginas;
+                        collection[4] = new SqlParameter("@FechaPublicacion", SqlDbType.VarChar);
+                        collection[4].Value = libro.FechaPublicacion;
+                        collection[5] = new SqlParameter("@IdEditorial", SqlDbType.Int);
+                        collection[5].Value = libro.Editorial.IdEditorial;
+                        collection[6] = new SqlParameter("@Edicion", SqlDbType.VarChar);
+                        collection[6].Value = libro.Edicion;
+                        collection[7] = new SqlParameter("@IdGenero", SqlDbType.Int);
+                        collection[7].Value = libro.Genero.IdGenero;
+
+
+                        //pasarle a mi command los parametros
+                        cmd.Parameters.AddRange(collection);
+
+                        //Abrir la conexión
+                        cmd.Connection.Open();
+
+                        //ejecutando nuestra sentencia
+                        int rowsAffected = cmd.ExecuteNonQuery();
+
+                        if (rowsAffected > 0)
+                        {
+                            result.Correct = true;
+                        }
+                        else
+                        {
+                            result.Correct = false;
+                        }
+
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                result.Correct = false;
+                result.ErrorMessage = ex.Message;
+                result.Ex = ex;
+            }
+            return result;
+        }
     }
 }
